b2c3483388ddccfe1d1dd43479cfda043782b5f9
hnzvbs <122280870+hnzvbs@users.noreply.github.com>
2023-01-19 17:21:10 +0700

    Add files via upload

---
+++ src/info.js
@@ -0,0 +1,14 @@
+const { chalk, inquirer, _, fs, instagram, print, delay } = require("./api.js");
+
+print(
+                    chalk`{bold.yellow
+  Find Bug ? Tell me on
+  —————————————————————————————————————————————————————
+  - Facebook   : https://fb.me/4RAEHAN
+  - Instagram  : https://instagram.com/hanzvibes
+  - GitHub     : https://github.com/hanzvibes
+  - Telegram   : https://t.me/InstaJS_Official
+  —————————————————————————————————————————————————————
+  
+  Thanks for your donation :)
+  \n}`);
